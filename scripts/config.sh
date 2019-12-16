0955db06fad47543b8addd3be1899d4bfea8c75b
John Lahut <lahut13@gmail.com>
2019-12-16 18:59:02 -0500

    adj config

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,2 +1,10 @@
+# point gopath to the project
 export GOPATH=$GOPATH:/Users/jlahut/UAlbany/parallel-computing/final-project/project
+
+# ssh into the rabbitmq node
 ssh -i ~/.ssh/mykey.rsa ec2-user@ec2-18-206-140-49.compute-1.amazonaws.com
+
+# ssh into the server node
+ssh -i ~/.ssh/mykey.rsa ec2-user@ec2-54-157-177-126.compute-1.amazonaws.com
+
+# ssh into a consumer node
